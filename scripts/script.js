0643b9e3542f88411975e7911f7cc5bd4bd889ff
Floortjevh <floortjevanhensbergen@gmail.com>
2025-09-22 15:48:40 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,11 @@
 // JavaScript Document
 console.log("hi");
+
+var deButton = document.querySelector("header nav button");
+
+deButton.onclick = toggleMenu;
+
+function toggleMenu() {  
+  var deNav = document.querySelector("header nav");
+  deNav.classList.toggle("toonMenu");
+}
